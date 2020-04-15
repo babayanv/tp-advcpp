019747112dadcb7fda3bc2e888503de214e88ac2
Babich <g8h2.babayan@yandex.ru>
2020-04-16 02:59:02 +0300

    added LinearAllocator class

---
+++ hw5/shmem/include/shmem/allocator/LinearAllocator.hpp
@@ -0,0 +1,55 @@
+#ifndef SHMEM_ALLOCATOR_LINEAR_ALLOCATOR_HPP
+#define SHMEM_ALLOCATOR_LINEAR_ALLOCATOR_HPP
+
+#include "shmem/SharedMemory.hpp"
+
+namespace shmem
+{
+
+namespace allocator
+{
+
+template <class T>
+class LinearAllocator
+{
+public:
+    using value_type = T;
+
+public:
+    constexpr LinearAllocator(SharedMemory* shmem)
+        : m_shmem(shmem)
+    {
+    }
+
+    constexpr LinearAllocator(const LinearAllocator<value_type>& other)
+        : m_shmem(other.m_shmem)
+    {
+    }
+
+    template <class U>
+    constexpr LinearAllocator(const LinearAllocator<U>& other)
+        : m_shmem(other.m_shmem)
+    {
+    }
+
+    ~LinearAllocator() = default;
+
+    value_type* allocate(std::size_t count)
+    {
+        return m_shmem->store<value_type>(count);
+    }
+
+    void deallocate(value_type* ptr, std::size_t count)
+    {
+        m_shmem->free(ptr, count);
+    }
+
+public:
+    SharedMemory* m_shmem;
+};
+
+} // namespace allocator
+
+} // namespace shmem
+
+#endif // SHMEM_ALLOCATOR_LINEAR_ALLOCATOR_HPP
