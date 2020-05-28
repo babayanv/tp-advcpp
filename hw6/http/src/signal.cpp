65756f4569df8edbdf7ef8cd3d000a06516ecfd8
Babich <g8h2.babayan@yandex.ru>
2020-05-28 03:37:45 +0300

    compilation error fix

--- hw6/http/src/signal.cpp
+++ hw6/http/src/signal.cpp
@@ -17,7 +17,7 @@ void Signal::register_handler(int signal, const Handler& handler)
 
 void Signal::handle(int signal)
 {
-    auto [ begin, end ] = std::equal_range(s_sig2handler.begin(), s_sig2handler.end(), signal);
+    auto [ begin, end ] = s_sig2handler.equal_range(signal);
     for (auto& elem = begin; elem != end; ++elem)
     {
         elem->second();
